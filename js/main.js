512b25040d8865dee76d2ae2ba01d2b0fa42a8ce
Dylan Jannetty <djannetty29@gmail.com>
2018-02-19 20:05:18 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,8 +1,27 @@
 console.log("Up and running!");
 
-var cardOne = "king";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "queen";
+var cards = ["queen", "queen", "king", "king"];
 
-console.log("User flipped " + cardTwo);
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length === 2){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry try again!");
+
+	}
+}
+
+
+
+
+
+console.log("User flipped " + cards[1]);
+console.log("User flipped " + cards[2]);
